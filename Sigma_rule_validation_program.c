a8bbac8df3e78dd130d71e34af7f1097d9b4065b
root-Brainoverflow <root@brainoverflow.kr>
2025-08-08 10:07:35 +0900

    Create Sigma_rule_validation_program.c

---
+++ Sigma_rule_validation_program.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h> // exit 위해 사용
+#include <string.h> // string 관련 function
+
+struct rule {
+    char *name;
+    char *status;
+    char *description;
+    char *references;
+    char *author;
+    char *date;
+    char *modified;
+    char *tags;
+    struct logsource *logsource;
+    struct detection *detection;
+    char *condition;
+    char *level;
+};
+
+struct logsource {
+    char *product;
+    char *category;
+};
+
+struct detection {
+    char *selection;
+    struct fields *fields;
+    char *condition;
+};
+
+struct fields {
+    char *name;
+    char *appendix;
+};
+
+struct rule parse_rule() {
+    struct rule r;
+    
+
+}
+
+
+int main() {
+    FILE *fp = NULL;
+    char fname[100];
+    printf("SIGMA RULE NAME(.yaml): ");
+    gets(fname);
+    if(strchr(".yaml", fname) == NULL) {
+        fprintf(stderr, "지정된 파일이 없습니다\n");
+        exit(1);        
+    }
+    fopen(fname, "r");
+    if(fp == NULL) {
+        fprintf(stderr, "지정된 파일이 없습니다\n");
+        exit(1);
+    }
+
+
+    fclose(fp);
+
+    return 0;
+}
